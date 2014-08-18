6ca5d68978d5643cff5dff306e0a1193fbabdb1e
zhaozhiming <kingzzm1982@sina.com>
2014-08-18 18:50:11 +0800

    add grid view

--- app/src/main/java/com/github/zzm/bushu/app/MainActivity.java
+++ app/src/main/java/com/github/zzm/bushu/app/MainActivity.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import android.widget.GridView;
+import android.widget.Toast;
+import com.github.zzm.bushu.app.adapter.ImageAdapter;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this, new String[]{"One", "Two", "Three", "Else"}));
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/github/zzm/bushu/app/adapter/ImageAdapter.java
@@ -0,0 +1,66 @@
+package com.github.zzm.bushu.app.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.github.zzm.bushu.app.R;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context context;
+    private String[] values;
+
+    public ImageAdapter(Context context, String[] values) {
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public int getCount() {
+        return values.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        if (convertView != null) return convertView;
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // get layout from book.xml
+        View gridView = inflater.inflate(R.layout.book, null);
+
+        // set value into textview
+        TextView textView = (TextView) gridView.findViewById(R.id.grid_item_label);
+        textView.setText(values[position]);
+
+        // set image based on selected text
+        ImageView imageView = (ImageView) gridView
+                .findViewById(R.id.grid_item_image);
+
+        String mobile = values[position];
+
+        if (mobile.equals("One")) {
+            imageView.setImageResource(R.drawable.one_logo);
+        } else if (mobile.equals("Two")) {
+            imageView.setImageResource(R.drawable.two_logo);
+        } else if (mobile.equals("Three")) {
+            imageView.setImageResource(R.drawable.three_logo);
+        } else {
+            imageView.setImageResource(R.drawable.else_logo);
+        }
+
+        return gridView;
+    }
+}
