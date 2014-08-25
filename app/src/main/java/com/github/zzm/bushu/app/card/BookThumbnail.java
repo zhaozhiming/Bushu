a40ca16de962b1406ace8ec4263ab46409a9ff79
zhaozhiming <kingzzm1982@sina.com>
2014-08-25 15:21:29 +0800

    remove xxhdpi density

--- app/src/main/java/com/github/zzm/bushu/app/card/BookThumbnail.java
+++ app/src/main/java/com/github/zzm/bushu/app/card/BookThumbnail.java
@@ -42,8 +42,6 @@ public class BookThumbnail extends CardThumbnail {
                 return Density.HDPI.name().toLowerCase();
             case DisplayMetrics.DENSITY_XHIGH:
                 return Density.XHDPI.name().toLowerCase();
-            case DisplayMetrics.DENSITY_XXHIGH:
-                return Density.XXHDPI.name().toLowerCase();
             default:
                 return Density.HDPI.name().toLowerCase();
         }
